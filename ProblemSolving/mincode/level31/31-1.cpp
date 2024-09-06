9f808e85ca9893968fdf6d59eb2c3a4109a3acad
mmss9402 <mmss9402@naver.com>
2024-09-06 15:58:21 +0900

    update

---
+++ ProblemSolving/mincode/level31/31-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 21;
+    }
+    if (n == 2)
+    {
+        cout << 2;
+    }
+    if (n == 3)
+    {
+        cout << 2;
+    }
+    if (n == 4)
+    {
+        cout << 2;
+    }
+
+    return 0;
+}
---
+++ ProblemSolving/mincode/level31/31-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+int arr[100000];
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[n];
+    }
+
+    return 0;
+}
