5c88cdf2c68757ad87622c2d551f843d65bf9178
mmss9402 <mmss9402@naver.com>
2025-01-08 15:24:09 +0900

    update

--- ProblemSolving/mincode/algorithm_top/union_find/uf6.cpp
+++ ProblemSolving/mincode/algorithm_top/union_find/uf6.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int myBoss['0'+ 100001];
 
+string str['0'+ 100001];
 
 int main(){
-    cout << int('0') << " " << int('F');
+    
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        
+    }
+
 
 
     return 0;
